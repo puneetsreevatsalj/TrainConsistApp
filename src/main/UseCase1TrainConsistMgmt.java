32526b62cf4a883aa15cc04c009c3faab9513684
Puneet SJ <puneetsreevatsalj@gmail.com>
2026-04-12 18:43:51 +0530

    Initial project setup

---
+++ src/main/UseCase1TrainConsistMgmt.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseCase1TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> train = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + train.size());
+    }
+}
